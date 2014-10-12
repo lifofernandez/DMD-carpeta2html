f66dad3697e8acf365afc9673ef16c87af15a958
artes multimediales <iuna.artes.multimediales@gmail.com>
2014-10-11 22:08:31 -0300

    colores unidades

--- generator.js
+++ generator.js
@@ -192,7 +192,7 @@ function generarPaginas(element_group,delta){
 
 	var headPagina = '<head><meta charset="utf-8"> <meta http-equiv="X-UA-Compatible" content="IE=edge"> <meta name="viewport" content="width=device-width, initial-scale=1"> <meta name="description" content=""> <meta name="keywords" content=""> <meta name="author" content="DMD/UVQ"> <link rel="icon" href="/favicon.ico"> <title>'+carpeta.titulo+' / '+paginaTitulo+'</title> <!-- DMD core CSS --> <link href="css/style.css" rel="stylesheet"><!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries --> <!--[if lt IE 9]> <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script> <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script> <![endif]--> </head>';
 	
-	var headerPagina = '<body class="page '+elementTipe+' '+elementTipe+'-'+elementDelta+'"><header class="navbar navbar-default navbar-fixed-top" role="banner">'+mainNav.render+'</header>';
+	var headerPagina = '<body class="page '+elementTipe+' '+elementTipe+'-'+delta+'"><header class="navbar navbar-default navbar-fixed-top" role="banner">'+mainNav.render+'</header>';
 
 	var indiceApartados = '<div class="col-md-3"><div class="dmd-sidebar hidden-print hidden-xs hidden-sm affix-top" role="complementary">'+sideNavFromElements(apartados,'apartado_titulo','',elementDelta,'subapartado', 'subapartado_titulo',paginaTitulo)+'</div></div>';
 
@@ -304,7 +304,7 @@ function getParts(element, what_to_get,parent_delta){
 
 			if(parts[i].getElementsByTagName(what_to_get+'_titulo')[0]){
 			var partDelta = parent_delta+'.'+(i+1);
-			var unidadDelta = '<div class="delta">'+partDelta+'</div>';
+			var unidadDelta = '<div class="delta '+what_to_get+'-delta">'+partDelta+'</div>';
 
 				var partId = makeUrl(parts[i].getElementsByTagName(what_to_get+'_titulo')[0].childNodes[0].nodeValue);
 
